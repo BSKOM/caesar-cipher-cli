2c57d2b9d9b0d038559870550e1d3f970b9545c5
BSKOM <b.shelestua@gmail.com>
2021-05-05 19:39:38 +0300

    feat: v1.0 encode logic

---
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -0,0 +1,12 @@
+
+let shift = 0;
+function encode(math, p) {
+  let codeS = (p.codePointAt(0) - 'a'.codePointAt(0) + 26 + (shift%26))%26
+  console.log(codeS)
+  return String.fromCodePoint(codeS)    
+}
+
+// function caesCipher(str, shift){
+//   str.replace(a-z, )
+
+// }

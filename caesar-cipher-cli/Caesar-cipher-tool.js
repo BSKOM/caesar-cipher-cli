7255507ba97c9029aea051d133d3a7aba033bd69
BSKOM <b.shelestua@gmail.com>
2021-05-05 19:44:18 +0300

    fix: fix logic - add code simbol a item

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -3,7 +3,7 @@ let shift = 0;
 function encode(math, p) {
   let codeS = (p.codePointAt(0) - 'a'.codePointAt(0) + 26 + (shift%26))%26
   console.log(codeS)
-  return String.fromCodePoint(codeS)    
+  return String.fromCodePoint(codeS + 'a'.codePointAt(0))
 }
 
 // function caesCipher(str, shift){

7c55ebc01c9d0a5f952dc9d6a0cd7c54e0ca81c4
BSKOM <b.shelestua@gmail.com>
2021-05-05 20:00:25 +0300

    feat: remake to one string

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -1,10 +1,9 @@
 
-let shift = 0;
-function encode(math, p) {
-  let codeS = (p.codePointAt(0) - 'a'.codePointAt(0) + 26 + (shift % 26)) % 26 + 'a'.codePointAt(0)
-  console.log(codeS)
-  return String.fromCodePoint(codeS)
+let shift = 7;
+function encoder(math, p) {
+  return String.fromCodePoint((p.codePointAt(0) - 'a'.codePointAt(0) + 26 + (shift % 26)) % 26 + 'a'.codePointAt(0))
 }
+console.log('This is secret. Message about "_" symbol!'.replace(/[a-z]*/, encoder))
 
 // function caesCipher(str, shift){
 //   str.replace(a-z, )

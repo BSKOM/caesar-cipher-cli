e27034935799268374ba2bcfac56b008f73c0bea
BSKOM <b.shelestua@gmail.com>
2021-05-05 20:21:45 +0300

    feat: add processing for uppercase simbols

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -1,9 +1,10 @@
 
 let shift = 7;
 function encoder(math, p) {
-  return String.fromCodePoint((p.codePointAt(0) - 'a'.codePointAt(0) + 26 + shift ) % 26 + 'a'.codePointAt(0))
+  let lowerUpper = p.codePointAt(0) > 96 ? lowerUpper : lowerUpper
+  return String.fromCodePoint((p.codePointAt(0) - lowerUpper + 26 + shift) % 26 + lowerUpper)
 }
-console.log('This is secret. Message about "_" symbol!'.replace(/[a-z]*/, encoder))
+console.log('This is secret. Message about "_" symbol!'.replace(/[a-zA-Z]*/, encoder))
 
 // function caesCipher(str, shift){
 //   str.replace(a-z, )

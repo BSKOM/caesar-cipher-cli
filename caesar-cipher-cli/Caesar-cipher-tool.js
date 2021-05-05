009681212aaa88a796dcd16be9bd3365027f94ef
BSKOM <b.shelestua@gmail.com>
2021-05-05 21:32:09 +0300

    feat:testing logic encoder function

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -1,11 +1,24 @@
 
 let shift = 7;
+let secret = 'This is secret. Message about "_" symbol!'
 function encoder(math, p) {
-  let lowerUpper = p.codePointAt(0) > 96 ? 'a'.codePointAt(0) : 'A'.codePointAt(0)
-  return String.fromCodePoint((p.codePointAt(0) - lowerUpper + 26 + shift) % 26 + lowerUpper)
+ 
+  let res = ''
+  for (let i = 0; i < secret.length; i++){
+    let lowerUpper = secret[i].codePointAt(0) > 96 ? 'a'.codePointAt(0) : 'A'.codePointAt(0)
+    if ((secret[i] >= 'a' && secret[i] <= 'z') || (secret[i] >= 'A' && secret[i] <= 'Z')){
+      let codeS = (secret[i].codePointAt(0) - lowerUpper + 26 + shift) % 26 + lowerUpper
+      res += String.fromCodePoint((secret[i].codePointAt(0) - lowerUpper + 26 + shift) % 26 + lowerUpper)
+    } else { 
+      res += secret[i]
+    }
+   }
+ return res
+  // return String.fromCodePoint((String(p).codePointAt(0) - lowerUpper + 26 + shift) % 26 + lowerUpper)
 }
-console.log('This is secret. Message about "_" symbol!'.replace(/[a-zA-Z]*/, encoder))
-
+console.log(encoder())
+// console.log(console.log('This is secret. Message about "_" symbol!'.replace(/([a-zA-Z]*)/, encoder)).replace(/([a-zA-Z]*)/g, encoder))
+// console.log('This is secret. Message about "_" symbol!'.replace(/([a-zA-Z]*)/g, encoder))
 // function caesCipher(str, shift){
 //   str.replace(a-z, )
 

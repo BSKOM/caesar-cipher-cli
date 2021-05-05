34aef27886f6279a0a52cd900e8af61de31351b0
BSKOM <b.shelestua@gmail.com>
2021-05-05 20:29:20 +0300

    fix: fix error in function

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -1,7 +1,7 @@
 
 let shift = 7;
-function encoder(match, p) {
-  let lowerUpper = p.codePointAt(0) > 96 ? lowerUpper : lowerUpper
+function encoder(math, p) {
+  let lowerUpper = p.codePointAt(0) > 96 ? 'a'.codePointAt(0) : 'A'.codePointAt(0)
   return String.fromCodePoint((p.codePointAt(0) - lowerUpper + 26 + shift) % 26 + lowerUpper)
 }
 console.log('This is secret. Message about "_" symbol!'.replace(/[a-zA-Z]*/, encoder))

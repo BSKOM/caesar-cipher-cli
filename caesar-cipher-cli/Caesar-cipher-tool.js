3feba9900ad2b1528ce78626e42fd08b9fc0f99e
BSKOM <b.shelestua@gmail.com>
2021-05-06 09:32:51 +0300

    feat: test with using replace

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -12,11 +12,14 @@ function encoder(math, p) {
     }
    }
  return res
-  // return String.fromCodePoint((String(p).codePointAt(0) - lowerUpper + 26 + shift) % 26 + lowerUpper)
 }
-console.log(encoder())
-// console.log(console.log('This is secret. Message about "_" symbol!'.replace(/([a-zA-Z]*)/, encoder)).replace(/([a-zA-Z]*)/g, encoder))
-// console.log('This is secret. Message about "_" symbol!'.replace(/([a-zA-Z]*)/g, encoder))
+// console.log(encoder())
+function replacer(p){
+  let lowerUpper = p.codePointAt(0) > 96 ? 'a'.codePointAt(0) : 'A'.codePointAt(0)
+  return String.fromCodePoint((secret[i].codePointAt(0) - lowerUpper + 26 + shift % 26) % 26 + lowerUpper) 
+}
+console.log(console.log('This is secret. Message about "_" symbol!'.replace(/([^\D]*)/g, replacer)))
+console.log('This is secret. Message about "_" symbol!'.replace(/([a-zA-Z]*)/g, encoder))
 // function caesCipher(str, shift){
 //   str.replace(a-z, )
 

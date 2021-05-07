fb9b26313c543d840d7c38830c377c72cb2a2a1c
BSKOM <b.shelestua@gmail.com>
2021-05-07 22:39:20 +0300

    feat: add parser function

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -1,55 +1,60 @@
-const {coder} = require('./coder.js')
+const {coder} = require('./coder')
+const { parser } = require('./parser')
 
-let args = process.argv.slice(2)
-let argAction = ''
-let argShift = undefined
-let argIn = stdin
-let argOut = stdout
-let argErr = stderr
-let countOpt = 0
-for(let i = 0; i<args.length; i++){
-  switch (args[i]) {
-    case '--action':
-    case '-a':
-      if ((args[i + 1] === 'encode' || args[i + 1] === 'decode')){
-        throw err
-      }
-      argAction = args[i + 1]
-      i += 1
-      countOpt += 1
-      break;
-    case '--shift':
-    case '-s':
-      if ((args[i + 1] === 'encode' || args[i + 1] === 'decode')) {
-        throw err
-      }
-      argShift = args[i + 1]
-      i += 1
-      countOpt += 1
-      break;
-    case '--input':
-    case '-i':
-      argIn = args[i + 1]
-      i += 1
-      countOpt += 1
-      break;
-    case '--output':
-    case '-o':
-      argOut = args[i + 1]
-      i += 1
-      countOpt += 1
-      break;
-    default:
-      break;
+function parser(params) {
+  let args = process.argv.slice(2)
+  let argAction = ''
+  let argShift = undefined
+  let argIn = stdin
+  let argOut = stdout
+  let argErr = stderr
+  let countOpt = 0
+  for (let i = 0; i < args.length; i++) {
+    switch (args[i]) {
+      case '--action':
+      case '-a':
+        if ((args[i + 1] === 'encode' || args[i + 1] === 'decode')) {
+          throw err
+          console.error('arg:action - param must be encode or decode only');
+        }
+        argAction = args[i + 1]
+        i += 1
+        countOpt += 1
+        break;
+      case '--shift':
+      case '-s':
+        argShift = args[i + 1]
+        i += 1
+        countOpt += 1
+        break;
+      case '--input':
+      case '-i':
+        argIn = args[i + 1]
+        i += 1
+        countOpt += 1
+        break;
+      case '--output':
+      case '-o':
+        argOut = args[i + 1]
+        i += 1
+        countOpt += 1
+        break;
+      default:
+        break;
+    }
   }
-}
-if (argAction.length === 0 ) {
-  throw err
-}
-if (typeof argShift === undefined) {
-  throw err
+
+  if (argAction.length === 0) {
+    throw err
+    console.error('arg: action - required option is empty');
+  }
+  if (typeof argShift === undefined) {
+    throw err
+    console.error('arg: shift - required option is empty');
+  }  
 }
 
-console.log(coder(7, 'This is secret. Message about "_" symbol!'))
 
-console.log(coder(-7, 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!'))
+console.log(coder('encode', 7, 'This is secret. Message about "_" symbol!'))
+
+console.log(coder('decode', 7, 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!'))
--- caesar-cipher-cli/coder.js
+++ caesar-cipher-cli/coder.js
@@ -1,8 +1,9 @@
-function coder(sh, s) {
+function coder(aarg, sh, s) {
+  let a = (aarg === 'encode') ? 1 : -1
   let coded = s.replace(/([A-Za-z])/g, (match) => {
     let lowerUpper = match.codePointAt(0) > 96 ? 'a'.codePointAt(0) : 'A'.codePointAt(0)
     let res = String.fromCodePoint(
-      (match.codePointAt(0) - lowerUpper + 26 + sh % 26) % 26 + lowerUpper)  
+      (match.codePointAt(0) - lowerUpper + 26 + anum * sh % 26) % 26 + lowerUpper)  
     return res
   })
   return coded 

32a9ac346c9abf204a56d72ea9e5a4477b48a8e7
BSKOM <b.shelestua@gmail.com>
2021-05-09 21:12:02 +0300

    feat:add stdin/stdout

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -8,7 +8,6 @@ const path = require('path')
 const filePathIn = path.join(__dirname, parser()[2])
 const filePathOut = path.join(__dirname, parser()[3])
 
-let myText = fs.readFileSync(filePathIn, 'utf-8')
+let myText = (parser()[2] !== undefined ) ? fs.readFileSync(filePathIn, 'utf-8') : process.stdin
 let myCodedText = coder((parser()[0] === 'encode' ? 1 : -1), parser()[1], myText)
-fs.appendFileSync(filePathOut, myCodedText )
-
+parser()[3] !== undefined ? fs.appendFileSync(filePathOut, myCodedText):process.stdout

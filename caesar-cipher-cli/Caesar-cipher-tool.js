55f7e4895c442f344ebf77d6706d9db64b41fae5
BSKOM <b.shelestua@gmail.com>
2021-05-09 09:35:50 +0300

    feat: optimized parser

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -1,7 +1,7 @@
 const {coder} = require('./coder')
 const {parser} = require('./parser')
 
-console.log(parser())
+console.log(parser()[0])
 
 
 console.log(coder('encode', 7, 'This is secret. Message about "_" symbol!'))
--- caesar-cipher-cli/parser.js
+++ caesar-cipher-cli/parser.js
@@ -1,57 +1,47 @@
 function parser() {
   let args = process.argv.slice(2)
-  let argAction = ''
-  let argShift = undefined
-  let argIn = ''
-  let argOut = ''
-  let allArg = []
-  for (let i = 0; i < args.length; i++) {
-    switch (args[i]) {
-      case '--action':
-      case '-a':
-        if ((args[i + 1] === 'encode' || args[i + 1] === 'decode')) {
-          throw err
-          console.error('arg:action - param must be encode or decode only');
-        }
-        argAction = args[i + 1]
-        i += 1
-        countOpt += 1
-        break;
-      case '--shift':
-      case '-s':
-        argShift = args[i + 1]
-        i += 1
-        countOpt += 1
-        break;
-      case '--input':
-      case '-i':
-        argIn = args[i + 1]
-        i += 1
-        countOpt += 1
-        break;
-      case '--output':
-      case '-o':
-        argOut = args[i + 1]
-        i += 1
-        countOpt += 1
-        break;
-      default:
-        break;
+  let allArg = ['', undefined, , ]
+  try {
+    for (let i = 0; i < args.length; i++) {
+      switch (args[i]) {
+        case '--action':
+        case '-a':
+          if (!(args[i + 1] === 'encode' || args[i + 1] === 'decode')) {
+            throw 'arg:action - param must be encode or decode only';
+          }
+          allArg[0] = args[i + 1]
+          i += 1
+          break;
+        case '--shift':
+        case '-s':
+          allArg[1] = args[i + 1]
+          i += 1
+           break;
+        case '--input':
+        case '-i':
+          allArg[2] = args[i + 1]
+          i += 1
+          break;
+        case '--output':
+        case '-o':
+          allArg[3] = args[i + 1]
+          i += 1
+          break;
+        default:
+          break;
+      }
     }
+    if (allArg[0].length === 0) {
+      throw 'arg: action - required option is empty';
+    }
+    if (typeof argShift === undefined) {
+      throw 'arg: shift - required option is empty';
+    }
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
   }
-
-  if (argAction.length === 0) {
-    throw err
-    console.error('arg: action - required option is empty');
-  }
-  if (typeof argShift === undefined) {
-    throw err
-    console.error('arg: shift - required option is empty');
-  }
-  allArg += argAction
-  allArg += argShift
-  allArg += argIn
-  allArg += argOut
   return allArg
 }
-module.export = { parser }
+
+module.exports = { parser }

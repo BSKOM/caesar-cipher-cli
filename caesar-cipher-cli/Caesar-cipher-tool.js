d5d1dea6ef7e6ca3f6dd18b0b2eac64b0f3c95b3
BSKOM <b.shelestua@gmail.com>
2021-05-09 08:48:15 +0300

    fix: fix module parser

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -1,58 +1,7 @@
 const {coder} = require('./coder')
-// const { parser } = require('./parser')
+const {parser} = require('./parser')
 
-function parser(params) {
-  let args = process.argv.slice(2)
-  let argAction = ''
-  let argShift = undefined
-  let argIn = stdin
-  let argOut = stdout
-  let argErr = stderr
-  let countOpt = 0
-  for (let i = 0; i < args.length; i++) {
-    switch (args[i]) {
-      case '--action':
-      case '-a':
-        if ((args[i + 1] === 'encode' || args[i + 1] === 'decode')) {
-          throw err
-          console.error('arg:action - param must be encode or decode only');
-        }
-        argAction = args[i + 1]
-        i += 1
-        countOpt += 1
-        break;
-      case '--shift':
-      case '-s':
-        argShift = args[i + 1]
-        i += 1
-        countOpt += 1
-        break;
-      case '--input':
-      case '-i':
-        argIn = args[i + 1]
-        i += 1
-        countOpt += 1
-        break;
-      case '--output':
-      case '-o':
-        argOut = args[i + 1]
-        i += 1
-        countOpt += 1
-        break;
-      default:
-        break;
-    }
-  }
-
-  if (argAction.length === 0) {
-    throw err
-    console.error('arg: action - required option is empty');
-  }
-  if (typeof argShift === undefined) {
-    throw err
-    console.error('arg: shift - required option is empty');
-  }  
-}
+console.log(parser())
 
 
 console.log(coder('encode', 7, 'This is secret. Message about "_" symbol!'))
--- caesar-cipher-cli/parser.js
+++ caesar-cipher-cli/parser.js
@@ -1,50 +1,57 @@
-let args = process.argv.slice(2)
-let argAction = ''
-let argShift = undefined
-let argIn = stdin
-let argOut = stdout
-let argErr = stderr
-let countOpt = 0
-for(let i = 0; i<args.length; i++){
-  switch (args[i]) {
-    case '--action':
-    case '-a':
-      if ((args[i + 1] === 'encode' || args[i + 1] === 'decode')){
-        throw err
-        console.error('arg:action - param must be encode or decode only');
-      }
-      argAction = args[i + 1]
-      i += 1
-      countOpt += 1
-      break;
-    case '--shift':
-    case '-s':
-      argShift = args[i + 1]
-      i += 1
-      countOpt += 1
-      break;
-    case '--input':
-    case '-i':
-      argIn = args[i + 1]
-      i += 1
-      countOpt += 1
-      break;
-    case '--output':
-    case '-o':
-      argOut = args[i + 1]
-      i += 1
-      countOpt += 1
-      break;
-    default:
-      break;
+function parser() {
+  let args = process.argv.slice(2)
+  let argAction = ''
+  let argShift = undefined
+  let argIn = ''
+  let argOut = ''
+  let allArg = []
+  for (let i = 0; i < args.length; i++) {
+    switch (args[i]) {
+      case '--action':
+      case '-a':
+        if ((args[i + 1] === 'encode' || args[i + 1] === 'decode')) {
+          throw err
+          console.error('arg:action - param must be encode or decode only');
+        }
+        argAction = args[i + 1]
+        i += 1
+        countOpt += 1
+        break;
+      case '--shift':
+      case '-s':
+        argShift = args[i + 1]
+        i += 1
+        countOpt += 1
+        break;
+      case '--input':
+      case '-i':
+        argIn = args[i + 1]
+        i += 1
+        countOpt += 1
+        break;
+      case '--output':
+      case '-o':
+        argOut = args[i + 1]
+        i += 1
+        countOpt += 1
+        break;
+      default:
+        break;
+    }
   }
-}
 
-if (argAction.length === 0 ) {
-  throw err
-  console.error('arg: action - required option is empty');
+  if (argAction.length === 0) {
+    throw err
+    console.error('arg: action - required option is empty');
+  }
+  if (typeof argShift === undefined) {
+    throw err
+    console.error('arg: shift - required option is empty');
+  }
+  allArg += argAction
+  allArg += argShift
+  allArg += argIn
+  allArg += argOut
+  return allArg
 }
-if (typeof argShift === undefined) {
-  throw err
-  console.error('arg: shift - required option is empty');
-}
+module.export = { parser }

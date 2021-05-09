05d144494b642b3f1b05da93ce483d89adf8cdb1
BSKOM <b.shelestua@gmail.com>
2021-05-09 12:36:47 +0300

    feat: add prototype transormStream

--- caesar-cipher-cli/Caesar-cipher-tool.js
+++ caesar-cipher-cli/Caesar-cipher-tool.js
@@ -1,7 +1,31 @@
-const {coder} = require('./coder')
-const {parser} = require('./parser')
+const { coder } = require('./coder')
+const { parser } = require('./parser')
+const { pipeline } = require('stream')
+const { Transform } = require('stream')
 const fs = require('fs')
-const path = require('path');
+const path = require('path')
+
+
+
+
+const transformStream = new Transform({
+  transform(chunk, _, done) {
+    let data = chunk.toString();
+    if (parser[0] === 'encode') 
+      this.push(coder(data, shift, 1))
+    else
+      this.push(coder(data, shift, -1))
+    done()
+  }
+})
+
+const outputStream
+
+console.log(__dirname)
+console.log(__filename)
+
+fs.appendFile()
+
 
 // path.format({
 //   root: '/ignored',
@@ -11,9 +35,22 @@ const path = require('path');
 
 // path.resolve('wwwroot', 'static_files/png/', '../gif/image.gif')
 
-console.log(parser()[0])
+// pipeline(
+//   input_stream, 
+//   transform_stream, 
+//   output_stream 
+// )
+// .then(success and error callbacks)
 
+// console.log(parser()[0])
 
-console.log(coder('encode', 7, 'This is secret. Message about "_" symbol!'))
+// If the input file is missed - use stdin as an input source.
+// If the output file is missed - use stdout as an output destination.
+// If the input and / or output file is given but doesn't exist or you can't read it(e.g.because of permissions or it is a directory) - human - friendly error should be printed in stderr.
+// If passed params are fine the output(file or stdout) should contain encoded / decoded content of input(file or stdin).
+// For encoding / decoding use only the English alphabet, all other characters should be kept untouched.
+// Using streams for reading, writing and transformation of text is mandatory.
 
-console.log(coder('decode', 7, 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!'))
+// console.log(coder('encode', 7, 'This is secret. Message about "_" symbol!'))
+
+// console.log(coder('decode', 7, 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!'))
